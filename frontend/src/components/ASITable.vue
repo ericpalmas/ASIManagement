23f4c748be13bc64ac8b476c9c3ecc9bf9ad2939
Eric Palmas <ericpalmas96@gmail.com>
2022-03-20 16:36:37 +0100

    Asi project view update

--- frontend/src/components/ASITable.vue
+++ frontend/src/components/ASITable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="course-table">
+  <!-- <div class="course-table">
     <button type="button" class="btn btn-info" @click="addNewRow">
       <i class="fas fa-plus-circle"></i>
     </button>
@@ -11,24 +11,13 @@
       <td>Semester</td>
     </tr>
     <tr v-for="(course, k) in courses" :key="k">
+
       <td>
         <input class="form-control" type="text" v-model="course.code" />
-        <!-- <input class="form-control" type="text" v-model="course[0]" /> -->
       </td>
       <td>
         <input class="form-control" type="text" v-model="course.module_title" />
-        <!-- <input class="form-control" type="text" v-model="course[1]" /> -->
       </td>
-      <!-- <td>
-        <input
-          class="form-control text-right"
-          type="number"
-          min="0"
-          step="1"
-          v-model="course.site"
-          @change="calculateLineTotal(course)"
-        />
-      </td> -->
       <td>
         <input class="form-control" type="text" v-model="course.site" />
       </td>
@@ -48,23 +37,95 @@
         </button>
       </td>
     </tr>
-    <tr></tr>
-    <!-- <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td>ects</td>
-      <td></td>
-    </tr>
+  </div> -->
 
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td>9</td>
-      <td></td>
-    </tr> -->
-  </div>
+  <table class="table table-light">
+    <!-- <thead>
+      <tr>
+        <td>Code</td>
+        <td>Module title</td>
+        <td>Site</td>
+        <td>ECTS</td>
+        <td>Semester</td>
+        <td></td>
+      </tr>
+    </thead> -->
+    <tbody v-for="(course, i) in courses" :key="i">
+      <tr>
+        <td colspan="5" class="table-active">
+          <label for="exampleColorInput" class="form-label">
+            {{ course.module_group }}
+          </label>
+        </td>
+        <td class="table-active">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            @click="addNewRow(i, course)"
+          >
+            <i class="fas fa-plus-circle"></i>
+          </button>
+        </td>
+      </tr>
+      <tr v-for="(item, k) in course.modules" :key="k">
+        <td colspan="5">
+          <select
+            class="form-select form-select-sm"
+            aria-label=".form-select-sm example"
+          >
+            <option
+              value="FTP_AdvAlgDS"
+              selected="item.code === FTP_AdvAlgDS ? true : false"
+            >
+              FTP_AdvAlgDS
+            </option>
+            <option
+              value="FTP_AppStat"
+              selected="item.code === FTP_AppStat ? true : false"
+            >
+              FTP_AppStat
+            </option>
+            <option
+              value="FTP_ApprAlg"
+              selected="item.code === FTP_ApprAlg ? true : false"
+            >
+              FTP_ApprAlg
+            </option>
+            <option value="FTP_BioEng" selected="true">
+              <b>Code:</b>
+              &nbsp;&nbsp; FTP_BioEng, Name:&nbsp;&nbsp; Biology, physiology and
+              anatomy for engineers, Site:&nbsp;&nbsp; LU, Credits:&nbsp;&nbsp;
+              3, Semester:&nbsp;&nbsp; 3
+            </option>
+          </select>
+        </td>
+        <!-- <td>
+          <input class="form-control" type="text" v-model="item.code" />
+        </td>
+        <td>
+          <input class="form-control" type="text" v-model="item.module_title" />
+        </td>
+        <td>
+          <input class="form-control" type="text" v-model="item.site" />
+        </td>
+        <td>
+          <input class="form-control" type="text" v-model="item.credits" />
+        </td>
+        <td>
+          <input class="form-control" type="text" v-model="item.semester" />
+        </td> -->
+        <td>
+          <button
+            type="button"
+            class="btn btn-danger"
+            @click="deleteRow(i, k, course)"
+          >
+            <i class="fas fa-trash"></i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 
@@ -81,15 +142,6 @@ export default {
       invoice_total: 0,
       invoice_tax: 5,
       courses: this.parametri
-      //   courses: [
-      //     {
-      //       code: "",
-      //       module_title: "",
-      //       site: "",
-      //       ects: "",
-      //       semester: 0,
-      //     },
-      //   ],
     }
   },
   methods: {
@@ -122,22 +174,35 @@ export default {
       }
       this.calculateTotal()
     },
-    deleteRow(index, course) {
-      var idx = this.courses.indexOf(course)
-      console.log(idx, index)
-      if (idx > -1) {
-        this.courses.splice(idx, 1)
-      }
-      this.calculateTotal()
+
+    // mettere controllo prima di cancellare una riga
+    deleteRow(i, k) {
+      console.log(i)
+      console.log(k)
+      //var obj = this.courses[i]
+      console.log(this.courses[i].modules[k])
+      this.courses[i].modules.splice(this.courses[i].modules[k], 1)
+      console.log(this.courses)
     },
-    addNewRow() {
-      this.courses.push({
+
+    addNewRow(i, course) {
+      console.log(course)
+      console.log(i)
+      // course.modules.push({
+      //   code: '',
+      //   module_title: '',
+      //   site: '',
+      //   ects: '',
+      //   semester: ''
+      // })
+      this.courses[i].modules.push({
         code: '',
         module_title: '',
         site: '',
         ects: '',
         semester: ''
       })
+      // console.log(this.courses[i].modules)
     }
   }
 }
--- frontend/src/views/AsiManagementView.vue
+++ frontend/src/views/AsiManagementView.vue
@@ -4,87 +4,90 @@
       <div class="card">
         <div class="card-body">
           <h1>ASI management</h1>
-          <b-card-text>Fundamental theoretical principles</b-card-text>
 
           <ASITable
             :parametri="[
               {
-                code: 'FTP_BioEng ',
-                module_title: 'Biology, physiology and anatomy for engineers',
-                site: 'LU',
-                credits: '3',
-                semester: '3'
+                module_group: 'FTP',
+                modules: [
+                  {
+                    code: 'FTP_BioEng ',
+                    module_title:
+                      'Biology, physiology and anatomy for engineers',
+                    site: 'LU',
+                    credits: '3',
+                    semester: '3'
+                  },
+                  {
+                    code: 'FTP_AppSta',
+                    module_title: 'Applied Statistics and Data Analysis',
+                    site: 'LU',
+                    credits: '3',
+                    semester: '2'
+                  },
+                  {
+                    code: 'FTP_MultiASys ',
+                    module_title: 'Multi-agent systems',
+                    site: 'LU',
+                    credits: '3',
+                    semester: '4'
+                  }
+                ]
               },
-              {
-                code: 'FTP_AppSta',
-                module_title: 'Applied Statistics and Data Analysis',
-                site: 'LU',
-                credits: '3',
-                semester: '2'
-              },
-              {
-                code: 'FTP_MultiASys ',
-                module_title: 'Multi-agent systems',
-                site: 'LU',
-                credits: '3',
-                semester: '4'
-              }
-            ]"
-          />
 
-          <b-card-text>Technical / scientific modules</b-card-text>
+              {
+                module_group: 'TSM',
+                modules: [
+                  {
+                    code: 'USI_UserExp ',
+                    module_title: 'User Experience Design',
+                    site: 'LU',
+                    credits: '6',
+                    semester: '2'
+                  },
+                  {
+                    code: 'USI_ProStyles ',
+                    module_title: 'Programming Styles',
+                    site: 'LU',
+                    credits: '3',
+                    semester: '1'
+                  },
+                  {
+                    code: 'TSM_AdvDataMgmt ',
+                    module_title: 'Advanced Data Management',
+                    site: 'LU',
+                    credits: '3',
+                    semester: '1'
+                  }
+                ]
+              },
 
-          <ASITable
-            :parametri="[
               {
-                code: 'USI_UserExp ',
-                module_title: 'User Experience Design',
-                site: 'LU',
-                credits: '6',
-                semester: '2'
-              },
-              {
-                code: 'USI_ProStyles ',
-                module_title: 'Programming Styles',
-                site: 'LU',
-                credits: '3',
-                semester: '1'
-              },
-              {
-                code: 'TSM_AdvDataMgmt ',
-                module_title: 'Advanced Data Management',
-                site: 'LU',
-                credits: '3',
-                semester: '1'
-              }
-            ]"
-          />
-
-          <b-card-text>Context modules</b-card-text>
-
-          <ASITable
-            :parametri="[
-              {
-                code: 'CM_QRM ',
-                module_title: 'Quality and risk management',
-                site: 'LU',
-                credits: '6',
-                semester: '2'
-              },
-              {
-                code: 'CM_AdvPM ',
-                module_title: 'Advanced project management',
-                site: 'LU',
-                credits: '3',
-                semester: '1'
-              },
-              {
-                code: 'CM_IntSust ',
-                module_title:
-                  'Integrated Sustainable Management of production systems',
-                site: 'LU',
-                credits: '3',
-                semester: '1'
+                module_group: 'CM',
+                modules: [
+                  {
+                    code: 'CM_QRM ',
+                    module_title: 'Quality and risk management',
+                    site: 'LU',
+                    credits: '6',
+                    semester: '2'
+                  },
+                  {
+                    code: 'CM_AdvPM ',
+                    module_title: 'Advanced project management',
+                    site: 'LU',
+                    credits: '3',
+                    semester: '1'
+                  },
+                  {
+                    code: 'CM_IntSust ',
+                    module_title:
+                      'Integrated Sustainable Management of production systems',
+                    site: 'LU',
+                    credits: '3',
+                    semester: '1'
+                  }
+                ]
               }
             ]"
           />
@@ -92,6 +95,46 @@
           <b-button title="Save file">
             <b-icon icon="cloud-upload" aria-hidden="true"></b-icon>
           </b-button>
+        </div>
+      </div>
+
+      <div id="cardsContainers" class="container pt-3 pb-4">
+        <div class="card">
+          <div class="card-body">
+            <h1>Corsi per ogni semestre</h1>
+
+            <table class="table table-light">
+              <thead>
+                <tr>
+                  <td>Semester</td>
+                  <td>N. Courses</td>
+                  <td>Credits</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>1</td>
+                  <td>6</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>3</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>1</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
@@ -102,67 +145,18 @@
 
 
 <script>
-//import AsiManagementTable from '../components/AsiManagementTable.vue'
 import ASITable from '../components/ASITable.vue'
-//import VuetableDemo from '../components/VuetableDemo.vue'
 
 export default {
   name: 'AsiManagementView',
   components: {
-    //AsiManagementTable,
     ASITable
-    //VuetableDemo,
   }
 }
 </script>
 
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .asiManagement {
-  margin: auto;
-  height: 60%;
-  width: 80%;
-  border: 5px solid #616161;
-  padding: 10px;
-  background-color: #ffffff;
-}
-.card {
-  margin-top: 10%;
-  margin-bottom: 10%;
-
-  margin-top: 4%;
-}
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #3f4254;
-}
-.card.card-custom {
-  border: none !important;
-  box-shadow: none !important;
-  width: 100%;
-  background-color: #ffffff !important;
-}
-
-.mb-6,
-.my-6 {
-  margin-bottom: 1.5rem !important;
-}
-.col-lg-6 {
-  flex: 0 0 50%;
-  max-width: 50%;
-} */
-
 .cardsContainers {
   min-height: 100vh;
   width: 60%;
